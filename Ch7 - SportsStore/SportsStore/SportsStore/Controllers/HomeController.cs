c44c6ed68f1443d485e6b5e4e6f550afbf6cc931
Kyle Givler <xvegan88x@gmail.com>
2021-09-19 18:56:33 -0400

    Start SportsStore Setup

---
+++ Ch7 - SportsStore/SportsStore/SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public int PageSize { get; set; } = 4;
+
+        private readonly IStoreRepository _storeRepository;
+
+        public HomeController(IStoreRepository storeRepository)
+        {
+            _storeRepository = storeRepository;
+        }
+
+        public ViewResult Index(int productPage = 1)
+        {
+            var vm = new ProductsListViewModel
+            {
+                Products = _storeRepository.Products
+                    .OrderBy(p => p.ProductId)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = _storeRepository.Products.Count()
+                }
+            };
+
+            return View(vm);
+        }
+    }
+}
