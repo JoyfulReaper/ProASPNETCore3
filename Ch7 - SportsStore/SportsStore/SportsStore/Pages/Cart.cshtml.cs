d6f657d9c66fcc550087b14f7b679c6899f8dcc4
Kyle Givler <xvegan88x@gmail.com>
2021-09-25 11:50:00 -0400

    Chapter 8

--- Ch7 - SportsStore/SportsStore/SportsStore/Pages/Cart.cshtml.cs
+++ Ch7 - SportsStore/SportsStore/SportsStore/Pages/Cart.cshtml.cs
@@ -4,13 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
 
 namespace SportsStore.Pages
 {
     public class CartModel : PageModel
     {
-        public void OnGet()
+        private readonly IStoreRepository _storeRepository;
+
+        public CartModel(IStoreRepository storeRepository)
         {
+            _storeRepository = storeRepository;
+        }
+
+        public Cart Cart { get; set; }
+        public string ReturnlUrl { get; set; }
+
+        public void OnGet(string returnUrl)
+        {
+            ReturnlUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+
+        public IActionResult OnPost(long productId, string returnId)
+        {
+            Product product = _storeRepository.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            Cart.AddItem(product, 1);
+            HttpContext.Session.SetJson("cart", Cart);
+            return RedirectToPage(new { ReturlUrl = ReturnlUrl });
         }
     }
 }
