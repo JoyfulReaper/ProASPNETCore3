99adc0323a3cb1d2ab1ffdceffeb8f6f8447f38a
Kyle Givler <xvegan88x@gmail.com>
2021-10-17 12:00:13 -0400

    Cookies

---
+++ Ch16 - PlatformFeatures2/Platform3/Platform3/ConsentMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Platform3
+{
+    public class ConsentMiddleware
+    {
+        private readonly RequestDelegate _nextDelegate;
+
+        public ConsentMiddleware(RequestDelegate nextDelegate)
+        {
+            _nextDelegate = nextDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if(context.Request.Path == "/consent")
+            {
+                ITrackingConsentFeature consentFeature
+                    = context.Features.Get<ITrackingConsentFeature>();
+                if(!consentFeature.HasConsent)
+                {
+                    consentFeature.GrantConsent();
+                } else
+                {
+                    consentFeature.WithdrawConsent();
+                }
+                await context.Response.WriteAsync(consentFeature.HasConsent ? "Consent Granted \n" : "Consent Withdrawn\n");
+            }
+            await _nextDelegate(context);
+        }
+    }
+}
---
+++ Ch16 - PlatformFeatures2/Platform3/Platform3/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Platform3
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(opts =>
+            {
+                opts.CheckConsentNeeded = context => true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseDeveloperExceptionPage();
+            app.UseCookiePolicy();
+            app.UseMiddleware<ConsentMiddleware>();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/cookie", async context =>
+                {
+                    int counter1 =
+                        int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
+                    context.Response.Cookies.Append("counter1", counter1.ToString(),
+                        new CookieOptions
+                        {
+                            MaxAge = TimeSpan.FromMinutes(30),
+                            IsEssential = true
+                        });
+
+                    int counter2 =
+                        int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
+                    context.Response.Cookies.Append("counter2", counter1.ToString(),
+                        new CookieOptions
+                        {
+                            MaxAge = TimeSpan.FromMinutes(30)
+                        });
+                    await context.Response
+                        .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
+                });
+
+
+                endpoints.MapGet("clear", context =>
+                {
+                    context.Response.Cookies.Delete("counter1");
+                    context.Response.Cookies.Delete("counter2");
+                    context.Response.Redirect("/");
+                    return Task.CompletedTask;
+                });
+
+                endpoints.MapFallback(async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
