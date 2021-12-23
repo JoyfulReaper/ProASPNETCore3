b3e1935331f7b7f494b639ca66b64c7e1678d50d
Kyle Givler <xvegan88x@gmail.com>
2021-12-22 22:05:02 -0500

    Chapter 9 - Part 2

---
+++ NET6/SportsStore/SportsStore/SpotsStore.Tests/OrderControllerTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SpotsStore.Tests
+{
+    public class OrderControllerTests
+    {
+        [Fact]
+        public void Cannot_Checkout_Empty_Cart()
+        {
+            // Arrange
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+            Cart cart = new Cart();
+            Order order = new Order();
+            OrderController target = new OrderController(mock.Object, cart);
+
+            //Act
+            ViewResult result = target.Checkout(order) as ViewResult;
+
+            // Assert
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never); // Check order has not been stored
+            Assert.True(string.IsNullOrEmpty(result.ViewName)); // Check method is returning the default view
+            Assert.False(result.ViewData.ModelState.IsValid); // Model is invalid
+        }
+
+        [Fact]
+        public void Cannot_Checkout_Invalid_Shipping_Details()
+        {
+            // Arrange
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+            Cart cart = new Cart();
+            cart.AddItem(new Product(), 1);
+            OrderController target = new OrderController(mock.Object, cart);
+            target.ModelState.AddModelError("error", "error");
+
+            //Act
+            ViewResult result = target.Checkout(new Order()) as ViewResult;
+
+            // Assert
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+            Assert.True(string.IsNullOrEmpty(result.ViewName)); // Check method is returning the default view
+            Assert.False(result.ViewData.ModelState.IsValid); // Model is invalid
+        }
+
+        [Fact]
+        public void Can_Checkout_And_Submit_Order()
+        {
+            // Arrange
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+            Cart cart = new Cart();
+            cart.AddItem(new Product(), 1);
+            OrderController target = new OrderController(mock.Object, cart);
+
+            // Act
+            RedirectToPageResult result = target.Checkout(new Order()) as RedirectToPageResult;
+
+            // Assert
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Once);
+            Assert.Equal("/Completed", result.PageName);
+        }
+    }
+}
