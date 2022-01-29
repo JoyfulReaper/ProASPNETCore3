8a103c73f324a5b0e41d9387f594e23a02635020
Kyle Givler <xvegan88x@gmail.com>
2022-01-29 16:19:13 -0500

    Chapter 21

---
+++ NET6/Ch18 - Example Project/WebApp/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DataContext _context;
+
+        public HomeController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(long id = 1)
+        {
+            Product prod = await _context.Products.FindAsync(id);
+            if (prod.CategoryId == 1)
+            {
+                return View("Watersports", prod);
+            }
+            else
+            {
+                return View(prod);
+            }
+        }
+
+        public IActionResult Common()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(_context.Products);
+        }
+    }
+}
