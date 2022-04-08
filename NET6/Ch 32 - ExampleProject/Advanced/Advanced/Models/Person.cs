3b525f4e0d4ea5183825c86c0558849dac3ed412
Kyle Givler <xvegan88x@gmail.com>
2022-04-07 20:10:10 -0400

    Chapter 36 - Blazor Forms - Part 1

--- NET6/Ch 32 - ExampleProject/Advanced/Advanced/Models/Person.cs
+++ NET6/Ch 32 - ExampleProject/Advanced/Advanced/Models/Person.cs
@@ -1,11 +1,25 @@
-﻿namespace Advanced.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Advanced.Models
 {
     public class Person
     {
         public long PersonId { get; set; }
+
+        [Required(ErrorMessage = "A firstname is required")]
+        [MinLength(3, ErrorMessage = "First name must be 3 or more characters")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "A firstname is required")]
+        [MinLength(3, ErrorMessage = "First name must be 3 or more characters")]
         public string LastName { get; set; }
+
+        [Required]
+        [Range(1, long.MaxValue, ErrorMessage = "A department must be selected")]
         public long DepartmentId { get; set; }
+
+        [Required]
+        [Range(1, long.MaxValue, ErrorMessage = "A location must be selected")]
         public long LocationId { get; set; }
 
         public Department Department { get; set; }
